0d3a304d5baabd4b67aa2a06d3469abcc9e62e7e
JoeGuillory <joeguillory15@gmail.com>
2025-03-26 16:57:10 -0500

    Started on Mouse Controls

--- Assets/Scripts/MouseControls.cs
+++ Assets/Scripts/MouseControls.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class MouseControls : MonoBehaviour
 {
+
+    [SerializeField]
+    private Camera _camera;
+
     Vector3 _worldPosition;
    
+    //Use a raycast from the main camera
+    // Make a camera variable to get a camera 
+    //Then cast a ray from the camera to see if we hit something
+    //Probably use the raycast thing to set the position of the object
+
     void Update()
     {
-        _worldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,Camera.main.nearClipPlane));
-        _worldPosition.z = 0;
-
-        transform.localPosition = _worldPosition;
+        
     }
 }
