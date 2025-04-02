8c4b1fd9e9a2e07a97f92050a6231c4f058db9de
JoeGuillory <joeguillory15@gmail.com>
2025-04-02 16:13:48 -0500

    Changed the bat controls
    Changed to a locked in type system. Need to implement  a multiply power by distance.
    
    The Ball Script was striped down.
    
    Added Kenny UI assets.

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -6,17 +6,12 @@ using UnityEngine;
 public class BallBehaviour : MonoBehaviour
 {
 
-    private Rigidbody _ballRigidbody;
-
-    [SerializeField]
-    private float _startPower = 10;
-
-
    
 
+ 
     private void Start()
     {
-        _ballRigidbody = GetComponent<Rigidbody>(); 
+      
     }
 
     private void Update()
@@ -26,13 +21,7 @@ public class BallBehaviour : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (_ballRigidbody)
-            _ballRigidbody.isKinematic = false;
 
-        if(collision.gameObject.tag == "Bat")
-            _ballRigidbody.AddForce((transform.right + (transform.up / 2)) * _startPower);
-
-        
     }
 
     
--- Assets/Scripts/BatControls.cs
+++ Assets/Scripts/BatControls.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using DG.Tweening;
 
 public class BatControls : MonoBehaviour
 {
@@ -10,31 +11,124 @@ public class BatControls : MonoBehaviour
     [SerializeField]
     private float _forceScaler;
 
+    [SerializeField]
+    private GameObject _ball;
+
+    [SerializeField]
+    private float _batPower = 30;
+    public float BatPower { get { return _batPower; } }
+
+    private float _maxRotationSpeed = 80;
+    private float _maxRotation = 80;
+    private float _maxBatPowerDistance = 3;
+    private float _maxBatPowerSpeed = 4;
+
+    private bool _rotationSet = false;
+    private bool _powerSet = false;
     private Rigidbody _rigidbody;
-    private HingeJoint _mouseJoint;
+
+    private float _direction = 1;
+    private float _amountMoved;
+    private bool _mouseClicked;
+    private bool _isSwung = false;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-        _mouseJoint = GetComponent<HingeJoint>();
+
 
     }
 
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
-        {
-            if (_rigidbody)
-                _rigidbody.AddTorque(-transform.right * _forceScaler * Time.deltaTime ,ForceMode.Impulse);
-             
+        _mouseClicked = false;
+        if(!_rotationSet)
+            BatRotation();
 
-            
+        if(_rotationSet && !_powerSet && !_mouseClicked)
+            if (Input.GetMouseButtonDown(0))
+                _powerSet = true;
+
+        if (_powerSet && _rotationSet && !_isSwung)
+        {
+            Invoke("SwingBat", 2);
+            _isSwung = true;
         }
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (_rotationSet && !_powerSet)
+            BatPowerMovement();
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            transform.DOKill();
+            other.attachedRigidbody.isKinematic = false;
+
+            other.attachedRigidbody.AddForce(other.transform.up + other.transform.right * _batPower, ForceMode.Impulse);
+
+        }
+
+
+    }
+
+
+    private void BatRotation()
+    {
+        
+        _amountMoved += _maxRotationSpeed * Time.deltaTime;
+        if(_amountMoved >= _maxRotation)
+        {
+            if (_direction == 1)
+                _direction = -1;
+            else
+                _direction = 1;
+
+            _amountMoved = 0;
+        }
+            
+        transform.RotateAround(_ball.transform.position, new Vector3(0, 0, 1), _maxRotationSpeed * _direction * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rotationSet = true;
+            _amountMoved = 0;
+            _direction = -1;
+            _mouseClicked = true;
+        }
+    }
+
+    private void BatPowerMovement()
+    {
+        _amountMoved += _maxBatPowerSpeed * Time.fixedDeltaTime;
+
+        if(_amountMoved >= _maxBatPowerDistance)
+        {
+            if (_direction == -1)
+                _direction = 1;
+            else
+                _direction = -1;
+            _amountMoved = 0;
+        }
+
+        _rigidbody.MovePosition(transform.position + ((_ball.transform.position - transform.position).normalized * _maxBatPowerSpeed * _direction * Time.fixedDeltaTime));
+ 
+    }
+
+    private void SwingBat()
+    {
+        transform.DOMove(_ball.transform.position, 0.5f).SetEase(Ease.InOutCirc);
     }
 }
